ad698657215345f7c7154ec1d5f2dfdbd49bb164
Dibyesh Sahoo <66529046+bosskaboss@users.noreply.github.com>
2024-10-16 21:49:10 -0700

    code
    with extra credit

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
+
     # do the rest...
 ]
 
@@ -22,15 +31,27 @@ for led in leds:
     led.direction = Direction.OUTPUT
 
 # main loop
+max = 0
+cycle = 0
 while True:
     volume = microphone.value
 
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    for i in range(0,11):
+        leds[i].value = 0
+    for i in range(max):
+        leds[i].value = 1
+        
+    #Extra credit
+    if max < volume//4367:
+        max = volume//4367
+    cycle = cycle + 1
+    if cycle > 30:
+        cycle = 0
+        max = max - 1
 
-    sleep(1)
+    sleep(.01)
 
     # instead of blinking,
     # how can you make the LEDs
